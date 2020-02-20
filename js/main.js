ffa2f1c67ab1c6e9ac326cc86b4e7683e2a8fef5
Luigi Fontana <luigifontana92@gmail.com>
2020-02-20 15:47:36 +0100

    Creazione Lista logica

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+    SCOPO DEL GIOCO: Fizz Buzz test:
+    Scrivi un programma che stampi i numeri da 1 a 100.
+    Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+    Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+*/
+
+// Creare un for per stampare i numeri da 1 a 100
+// Controllare se il numero stampato è divisibile per 3
+// Se si sostituirlo con Fizz
+// Controllare se il numero stampato è divisibile per 5
+// Se si sostituirlo con Buzz
+// Controllare se il numero è divisibile sia per 3 che per 5
+// Se si sostituirlo con FizzBuzz
